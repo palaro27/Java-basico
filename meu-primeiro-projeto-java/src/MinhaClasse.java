2198797dc794c7db8ad69f66d67dd85048338510
Hugo Dias <hugocdias27@gmail.com>
2025-04-08 20:32:14 -0300

    minha primeira classe

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -1,7 +1,13 @@
 public class MinhaClasse {
     public static void main (String [] args) {
+        String primeiroNome = "Hugo";
+        String segundoNome = "Dias";
 
-        System.out.print ("Olá, mundo!");
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
 
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
