85a8dcc0704490ebce9a540aabf08eb9cfb6fdc2
Hugo Dias <hugocdias27@gmail.com>
2025-04-08 21:05:17 -0300

    criando projeto para tipos de variaveis

--- meu-primeiro-projeto-java/src/edu/hugo/primeirasemana/BoletimEstudantil.java
+++ meu-primeiro-projeto-java/src/edu/hugo/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.hugo.primeirasemana;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 8;
--- meu-primeiro-projeto-java/src/edu/hugo/primeirasemana/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/edu/hugo/primeirasemana/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.hugo.primeirasemana;
 public class MinhaClasse {
     public static void main (String [] args) {
         String primeiroNome = "Hugo";
