aadce8fbf5c73b72d4a35ecefcc020346b5eb50f
Hugo Dias <hugocdias27@gmail.com>
2025-04-09 21:24:46 -0300

    Exercicio Smart Tv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.mudarCanal(12);
+        System.out.println("Novo Canal: " + smartTv.canal);
+    }
+}
