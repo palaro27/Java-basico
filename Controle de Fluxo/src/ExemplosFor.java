4a668ce22570c79a6d1814aae54d80f44db5e0df
Hugo Dias <hugocdias27@gmail.com>
2025-05-02 08:47:22 -0300

    Exemplos For

---
+++ Controle de Fluxo/src/ExemplosFor.java
@@ -0,0 +1,8 @@
+public class ExemplosFor {
+    public static void main(String[] args){
+        for(int carneirinhos = 1; carneirinhos <=20; carneirinhos++){
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+        System.out.println("João dormiu");
+    }
+}
